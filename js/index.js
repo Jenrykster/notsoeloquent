a55b07e2d2d136bbcda501dc1538161a8d2d297b
João Henrique <joaoenrique.san@gmail.com>
2021-05-16 13:47:12 -0300

    Finish chapter one

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 console.log("Hello World !");
+
+console.log("The escape character for new-line is \"\\n\""); // The "\" can be used to display special characters like a tab (\t)
+
+console.log(Infinity + " and " + -Infinity + " are actually number values"); 
+
+console.log("I can also use scientific notation like: " + 2e-3 + " which is represented like \"2e-3\""); // This is the same as 0.002
+
+console.log("I can use typeof to get the type of data, ex: " + typeof Infinity + " is the type of Infinity");
+
+console.log(`Boolean operators are cool ! is 2 more than one ? ${2 > 1} it is !`); 
+
+console.log("Ternary operators are cool too. " + (1 > 2? "1 is bigger" : "two is bigger")); //This is a conditional ternary operator
